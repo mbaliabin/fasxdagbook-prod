2a7c91a3690b4a36967a12a1415a1a7969cf253e
baliabin.maksim <baliabin.maksim@gmail.com>
2025-12-25 22:42:40 +0300

    5

--- fasx-frontend/vite.config.js
+++ fasx-frontend/vite.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+      allowedHosts: ['fasx.pro'],
+      host: true,
+      port: 5173,
+      strictPort: true,
+    },
 })
